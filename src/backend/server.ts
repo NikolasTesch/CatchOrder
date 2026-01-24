acd0e1125b79449c18de5ae586f5ff0f6b188875
davims9 <davimourasouza@gmail.com>
2026-01-24 17:19:37 -0300

    chore: initial project setup and configurations
    - Create folder structure for MVC architecture (backend/frontend)
    - Initialize package.json and install dependencies (Express, SQLite, TypeScript)
    - Configure TypeScript (tsconfig.json) and VS Code environment
    - Implement initial server entry points (server.ts, app.ts)

---
+++ src/backend/server.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import { app } from './app';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Acesse: http://localhost:${PORT}`);
+});

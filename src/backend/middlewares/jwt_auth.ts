bd16f5c87489fa9b9fb30ca32f82ead74a47cd34
FIs <carvalhofabily@gmail.com>
2026-01-26 12:41:19 -0300

    feat:JWT Auth e .env.example

---
+++ src/backend/middlewares/jwt_auth.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface UserPayload {
+  id: string;
+  email: string;
+  [key: string]: any; //Futuramente deve ser trocado para os campos que serão usados no payload - Da forma que está é uma forma genérica
+}
+
+//Declaração global para adicionar a propriedade user (opcional) ao objeto Request (que faz parte do express)
+declare global {
+  namespace Express {
+    interface Request {
+      user?: UserPayload;
+    }
+  }
+}
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction): void => {
+  const token = req.cookies.token || req.headers['authorization']?.split(' ')[1];
+
+  if (!token) {
+    res.status(401).json({ message: 'Access denied. No token provided.' });
+    return;
+  }
+
+  try {
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      console.error('JWT_SECRET is not defined in environment variables.');
+      res.status(500).json({ message: 'Internal Server Error' });
+      return;
+    }
+
+    const decoded = jwt.verify(token, secret) as UserPayload;
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(403).json({ message: 'Invalid token.' });
+    return;
+  }
+};

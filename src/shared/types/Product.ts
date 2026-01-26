0597ae28c116a736096c9aea66e6cdad9908bd87
Romulo Reis <romuloreis06@gmail.com>
2026-01-26 17:36:49 -0300

    :sparkles: feat(products): add Product model with CRUD operations

---
+++ src/shared/types/Product.ts
@@ -0,0 +1,9 @@
+export interface Product {
+  id: string;
+  category_id: string;
+  name: string;
+  description: string | null;
+  price: number;
+  image_path: string | null;
+  is_active: boolean;
+}

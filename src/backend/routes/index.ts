29ce276f6357b6fc6895ecc766e08bbe8d63da9a
Romulo Reis <romuloreis06@gmail.com>
2026-01-27 17:12:12 -0300

    test(products): add unit and integration coverage for model and controller

--- src/backend/routes/index.ts
+++ src/backend/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { usersRoutes } from './userRoutes';
 import { ordersRoutes } from './orderRoutes';
 import { tablesRoutes } from './tableRoutes';
+import { productsRoutes } from './productRoutes';
 
 const routes = Router();
 
 routes.use('/users', usersRoutes);
 routes.use('/orders', ordersRoutes);
 routes.use('/tables', tablesRoutes);
+routes.use('/products', productsRoutes);
 
 export { routes };

2007bbbe814d3024b3e6c8be82b7add2cc1f2292
Romulo Reis <romuloreis06@gmail.com>
2026-01-28 21:30:40 -0300

    fix: remove unecessary middleware from authRoutes

--- src/backend/routes/authRoutes.ts
+++ src/backend/routes/authRoutes.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 import authController from '../controllers/authController';
-import { authenticateToken } from "../middlewares/jwtAuth";
 
 const authRoutes = Router();
 
-// Rotas protegidas
-authRoutes.post('/logout', authenticateToken, authController.logout.bind(authController));
-authRoutes.get('/me', authenticateToken, authController.me.bind(authController));
+authRoutes.post('/logout', authController.logout.bind(authController));
+authRoutes.get('/me', authController.me.bind(authController));
 
 export { authRoutes };

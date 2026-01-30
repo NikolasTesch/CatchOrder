0e09bd0b42cb1977caf4d688bec1204e7b35c97b
FIs <carvalhofabily@gmail.com>
2026-01-30 20:37:44 -0300

    fix: VPN server implementation

--- webpack.config.ts
+++ webpack.config.ts
@@ -53,6 +53,7 @@ const config: webpack.Configuration = {
   output: {
     path: path.resolve(__dirname, 'public/js'),
     filename: '[name].bundle.js',
+    publicPath: '../',
     clean: true,
   },
   plugins: [
